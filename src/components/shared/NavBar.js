44b74f9a670f722a80e11d42becb50e496e5426a
bornittah <bornittah@gmail.com>
2022-07-27 16:24:49 +0300

    Add NavLinks

--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import logo from '../../space-logo.png';
 
-const NavBar = () => (
-  <header className="header">
-    <nav className="navbar">
-      <div>
-        <h2>Space Travelers Hub </h2>
-      </div>
-      <ul className="nav-menu list">
-        <li className="nav-link">
-          <Link to="/">Rockets</Link>
-        </li>
-        <li className="nav-link">
-          <Link to="/missions"> Missions</Link>
-        </li>
-        <li className="nav-link border-left">
-          <Link to="/profile">My Profile</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const NavBar = () => {
+  const activeClassName = 'nav-link-active';
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <h2>Space Travelers Hub </h2>
+        </div>
+        <ul className="nav-menu list">
+          <li className="nav-link">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li className="nav-link">
+            <NavLink
+              to="/missions"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li className="nav-link border-left">
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
